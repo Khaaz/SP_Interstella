6b4b268cac159bb199083fea18b604748012db3b
larossi <L-Armand.ROSSI@etu.uca.fr>
2018-12-04 11:54:23 +0100

    Skins foldered

--- src/Configuration/Path.java
+++ src/Configuration/Path.java
@@ -3,6 +3,6 @@ package Configuration;
 public class Path {
     public static final String asset = "file:assets/";
 
-    public static final String Shep = Path.asset + "Shep/Shep.png";
-    public static final String enemy = Path.asset + "Shep/enemy";
+    public static final String Shep = Path.asset + "Shep/Sheps.png";
+    public static final String enemy = Path.asset + "Foe/slashturned";
 }
