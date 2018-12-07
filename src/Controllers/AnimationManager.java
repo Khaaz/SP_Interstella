d0be04795e00cc0ba461d043a4bc868eb16b978e
lobellec <louison.bellec@etu.uca.fr>
2018-12-07 18:17:46 +0100

    .

---
+++ src/Controllers/AnimationManager.java
@@ -0,0 +1,16 @@
+package Controllers;
+
+import javafx.animation.AnimationTimer;
+
+public class AnimationManager extends AnimationTimer {
+
+    public AnimationManager() {
+        super();
+
+    }
+
+    @Override
+    public void handle(long l) {
+
+    }
+}
--- src/Controllers/Scenes/GameScene.java
+++ src/Controllers/Scenes/GameScene.java
@@ -2,7 +2,6 @@ package Controllers.Scenes;
 
 import Class.Entities.AEntity;
 import Views.Roots.GameRoot;
-import javafx.event.EventHandler;
 import javafx.scene.Cursor;
 import javafx.scene.input.MouseEvent;
 
@@ -14,34 +13,9 @@ public class GameScene extends AScene {
 
         AEntity Shep = ((GameRoot)this.getRoot()).Shep;
 
-        /*this.setOnMouseMoved(new EventHandler<MouseEvent>(){
-            @Override
-            public void handle(MouseEvent event) {
-                Shep.body.setX(event.getSceneX() - (Shep.body.getFitWidth()/2));
-                Shep.body.setY(event.getSceneY() - (Shep.body.getFitHeight()/2));
-            }
-        });*/
-
         this.setOnMouseMoved(event -> onMouseMoved(Shep, event));
         this.setOnMouseDragged(event -> onMouseMoved(Shep, event));
 
-        this.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                Shep.body.setX(event.getSceneX() - (Shep.body.getFitWidth()/2));
-                Shep.body.setY(event.getSceneY() - (Shep.body.getFitHeight()/2));
-            }
-        });
-
-        /*this.setOnMousePressed(new EventHandler<MouseEvent>(){
-            @Override
-            public void handle(MouseEvent event) {
-                root.getChildren().add(
-                        new Bullet(event.getSceneX(),
-                                s2.body.getY())
-                );
-            }
-        });*/
     }
 
     private void onMouseMoved(AEntity Shep, MouseEvent event) {
