0266311c3efe367a86f6d9e6b5d9f081d856ffa0
Louis <louis73420@hotmail.fr>
2019-01-15 19:01:04 +0100

    added background immages to directory + corrected paths

--- src/Views/Components/ButtonCpnt.java
+++ src/Views/Components/ButtonCpnt.java
@@ -5,6 +5,7 @@ import javafx.scene.text.Font;
 
 public class ButtonCpnt extends Button {
     public ButtonCpnt() {
+
         this.setFont(Font.loadFont("file:assets/Fonts/Daft_Font.TTF", 25));
         this.setMinWidth(300);
         this.setMinHeight(50);
--- src/Views/Components/LabelCpnt.java
+++ src/Views/Components/LabelCpnt.java
@@ -5,6 +5,7 @@ import javafx.scene.text.Font;
 
 public class LabelCpnt extends Label {
     public LabelCpnt(){
+
         this.setFont(Font.loadFont("file:assets/Fonts/Daft_Font.TTF", 50));
     }
 }
